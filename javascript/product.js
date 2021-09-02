f3d4a05aebfd11dc1d8746e6b2c91b7d677496e8
ailin <aili0057@stud.kea.dk>
2021-09-02 16:54:13 +0200

    add sold out and off tag in product list

--- javascript/product.js
+++ javascript/product.js
@@ -2,18 +2,24 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 url = "https://kea-alt-del.dk/t7/api/products/" + id;
 fetch(url)
-.then((res)=>res.json())
-.then((data)=>showProduct(data));
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
 
 function showProduct(product) {
-    console.log(product);
-    console.log(product.productdisplayname);
-    document.querySelector("#product-name").textContent = product.productdisplayname;
-    document.querySelector("#prod-img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-    document.querySelector("#prod-img").alt = product.productdisplayname;
-    document.querySelector("#prod-price").textContent = `${product.price}kr.`;
-    document.querySelector("#prod-color").textContent = `Color: ${product.basecolour}`;
-    document.querySelector("#inv-nr").textContent = `Ineventory Number: ${product.price}`;
-    document.querySelector("#specs").innerHTML = `${product.description}`;
-
-} 
+  console.log(product);
+  console.log(product.productdisplayname);
+  document.querySelector("#product-name").textContent =
+    product.productdisplayname;
+  document.querySelector(
+    "#prod-img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector("#prod-img").alt = product.productdisplayname;
+  document.querySelector("#prod-price").textContent = `${product.price}kr.`;
+  document.querySelector(
+    "#prod-color"
+  ).textContent = `Color: ${product.basecolour}`;
+  document.querySelector(
+    "#inv-nr"
+  ).textContent = `Ineventory Number: ${product.id}`;
+  document.querySelector("#specs").innerHTML = `${product.description}`;
+}
--- product-list.js
+++ product-list.js
@@ -3,48 +3,64 @@ const cat = urlParams.get("categories");
 url = "https://kea-alt-del.dk/t7/api/products?category=" + cat;
 
 fetch(url)
-.then(function(res){
+  .then(function (res) {
     return res.json();
-})
-.then(function(data){
+  })
+  .then(function (data) {
     handleProductList(data);
-})
+  });
 
-function handleProductList(data){
-    console.log(data);
-    data.forEach(showProductCard);
+function handleProductList(data) {
+  console.log(data);
+  data.forEach(showProductCard);
 }
 
 function showProductCard(prod) {
-    console.log(prod);
-    const shCard = document.querySelector("#prod-card-template").content;
-    const clone = shCard.cloneNode(true);
-    clone.querySelector("#prod-link").setAttribute("href", "product.html?id=" + prod.id);
-    clone.querySelector("#card-title").textContent = prod.productdisplayname;
-    clone.querySelector("#brand-name").textContent = prod.brandname;
-    clone.querySelector("#price").textContent = `${prod.price} kr.`;
-    clone.querySelector("#prod-pic").src = `https://kea-alt-del.dk/t7/images/webp/640/${prod.id}.webp`;
+  console.log(prod);
+  const shCard = document.querySelector(".prod-card-template").content;
+  const clone = shCard.cloneNode(true);
+  clone
+    .querySelector(".prod-link")
+    .setAttribute("href", "product.html?id=" + prod.id);
+  clone.querySelector(".card-title").textContent = prod.productdisplayname;
+  clone.querySelector(".brand-name").textContent = prod.brandname;
+  clone.querySelector(".price").textContent = `${prod.price} kr.`;
+  clone.querySelector(
+    ".prod-pic"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${prod.id}.webp`;
 
-    if (prod.soldout) {
-        clone.querySelector("#prod-card").classList.add("sold-out");
-    }
-    if (prod.discount) {
-        clone.querySelector("#off").textContent = `${Math.ceil(prod.price * (1 - prod.discount / 100))} kr.`;
-        clone.querySelector("#price").classList.add("old-price");
-    }
-    const parent = document.querySelector("section");
-    parent.appendChild(clone);
+  if (prod.soldout) {
+    clone.querySelector(".prod-card").classList.add("sold-out");
+  } else {
+    clone.querySelector(".sold-out-tag").remove();
+  }
+  if (prod.discount) {
+    clone.querySelector(".off").textContent = `${Math.ceil(
+      prod.price * (1 - prod.discount / 100)
+    )} kr.`;
+    clone.querySelector(".off-tag").textContent = `${prod.discount}% OFF`;
+    clone.querySelector(".price").classList.add("old-price");
+  } else {
+    clone.querySelector(".off-tag").remove();
+  }
+  const parent = document.querySelector("section");
+  parent.appendChild(clone);
 }
 
-
-{/* <template id="prod-card-template">
-<div id="prod-card">
-    <a id="prod-link" href="">
-        <img id="prod-pic" src="assets/black-shirt.webp">
+{
+  /* <template class="prod-card-template">
+<div class="prod-card">
+    <a class="prod-link" href="">
+        <div class="container">
+            <img class="prod-pic" src="" alt="">
+            <div class="sold-out-tag">Sold Out</div>
+            <div class="off-tag">47% OFF</div>
+        </div>
     </a>
-    <a id="card-title" href="product.html"> Black Leaping Cat T-shirt</a>
-        <p id="brand-name">	</p>
-        <p id="price">899 kr.</p>
-        <p id="off"></p>
-    </div>
-</template> */}
+    <a class="card-title" href="product.html"> Black Leaping Cat T-shirt</a>
+    <p class="brand-name"> </p>
+    <p class="price">899 kr.</p>
+    <p class="off"></p>
+</div>
+</template> */
+}
