67ad81b99bf1f73dcd68914f82f04f68c5063238
ailin <aili0057@stud.kea.dk>
2021-09-02 15:38:29 +0200

    change id for classes

--- category.js
+++ category.js
@@ -1,28 +1,29 @@
 const url = "https://kea-alt-del.dk/t7/api/categories";
 
 fetch(url)
-.then(function(res){
+  .then(function (res) {
     return res.json();
-})
-.then(function(data){
+  })
+  .then(function (data) {
     handleCategories(data);
-})
+  });
 
-function handleCategories(data){
-    // console.log(data);
-    data.forEach(showCategories);
+function handleCategories(data) {
+  // console.log(data);
+  data.forEach(showCategories);
 }
 
 function showCategories(cat) {
-    console.log(cat);
-    const catCard = document.querySelector("#cat-template").content;
-    const clone = catCard.cloneNode(true);
-    clone.querySelector(".cat-card").setAttribute("href", "product-list.html?categories=" + cat.category);
-    clone.querySelector(".cat-title").textContent = cat.category;
-    const parent = document.querySelector("section");
-    parent.appendChild(clone);
+  console.log(cat);
+  const catCard = document.querySelector(".cat-template").content;
+  const clone = catCard.cloneNode(true);
+  clone
+    .querySelector(".cat-card")
+    .setAttribute("href", "product-list.html?categories=" + cat.category);
+  clone.querySelector(".cat-title").textContent = cat.category;
+  const parent = document.querySelector("section");
+  parent.appendChild(clone);
 }
-
 
 /* <template id="cat-template">
 <a href="" class="cat-card">
